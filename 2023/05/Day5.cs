d928729de8508db5116886d57e4c58e51beda329
JBillson <justin.billson@gmail.com>
2023-12-12 14:16:10 +0200

    day 5 part two, unable to complete, out of memory exception.  Need to find a better way of doing this.

--- 2023/05/Day5.cs
+++ 2023/05/Day5.cs
@@ -15,7 +15,30 @@ public class Day5
             return;
         }
 
-        var seeds = input[0].Split(':')[1].Trim().Split(' ').Select(long.Parse).ToList();
+        BruteForceMethod(part, input);
+    }
+
+    private static void BruteForceMethod(Program.Part part, List<string> input)
+    {
+        var seeds = new List<long>();
+        if (part == Program.Part.PartOne)
+        {
+            seeds = input[0].Split(':')[1].Trim().Split(' ').Select(long.Parse).ToList();
+        }
+        else
+        {
+            var seedInput = input[0].Split(':')[1].Trim().Split(' ').Select(long.Parse).ToList();
+            for (var i = 0; i < seedInput.Count; i += 2)
+            {
+                var start = seedInput[i];
+                var count = seedInput[i + 1];
+                for (var j = start; j < start + count; j++)
+                {
+                    seeds.Add(j);
+                }
+            }
+        }
+
         input.RemoveRange(0, 2);
 
         var maps = new Dictionary<string, List<MapInstruction>>();
@@ -47,39 +70,48 @@ public class Day5
             orderedMaps.Add(key, map.OrderBy(x => x.SourceRangeStart).ToList());
         }
 
-        foreach (var seed in seeds)
+        Console.WriteLine($"Evaluating {seeds.Count} seeds");
+        for (var i = 0; i < seeds.Count; i++)
         {
+            var seed = seeds[i];
             var value = seed;
             foreach (var (key, map) in maps)
             {
-                Console.WriteLine($"Map: {key}");
+                if (seed == 0)
+                    Console.WriteLine($"Map: {key}");
                 var originalVal = value;
                 foreach (var instruction in map)
                 {
-                    Console.WriteLine(
-                        $"Checking value {originalVal} -> {instruction.SourceRangeStart}:{instruction.SourceRangeStart + instruction.RangeLength}");
-                    if (originalVal >= instruction.SourceRangeStart &&
-                        originalVal <= instruction.SourceRangeStart + instruction.RangeLength)
-                    {
+                    if (seed == 0)
                         Console.WriteLine(
-                            $"Value {originalVal} falls between {instruction.SourceRangeStart}:{instruction.SourceRangeStart + instruction.RangeLength}");
+                            $"Checking value {originalVal} -> {instruction.SourceRangeStart}:{instruction.SourceRangeStart + instruction.RangeLength}");
+                    if (originalVal >= instruction.SourceRangeStart &&
+                        originalVal < instruction.SourceRangeStart + instruction.RangeLength)
+                    {
+                        if (seed == 0)
+                            Console.WriteLine(
+                                $"Value {originalVal} falls between {instruction.SourceRangeStart}:{instruction.SourceRangeStart + instruction.RangeLength}");
                         var diff = originalVal - instruction.SourceRangeStart;
                         value = instruction.DestinationRangeStart + diff;
                         var sourceMap = key.Split('-')[0];
                         var destinationMap = key.Split('-')[2];
-                        Console.WriteLine($"{sourceMap} {originalVal} -> {destinationMap} {value}");
+                        if (seed == 0)
+                            Console.WriteLine($"{sourceMap} {originalVal} -> {destinationMap} {value}");
                     }
                 }
             }
 
             locationValues.Add(value);
+            Console.WriteLine($"{i + 1}/{seeds.Count} seeds evaluated");
         }
 
         Console.WriteLine(JsonConvert.SerializeObject(locationValues));
-        Console.WriteLine($"Part One: {locationValues.Min()}");
+        Console.WriteLine(part == Program.Part.PartOne
+            ? $"Part One: {locationValues.Min()}"
+            : $"Part Two: {locationValues.Min()}");
     }
 
-    public class MapInstruction
+    private class MapInstruction
     {
         public long DestinationRangeStart { get; set; }
         public long SourceRangeStart { get; set; }
